87af8e4bd91338a028016d1c14783e19e4fa01b1
ialikh72 <ialikh.w1@gmail.com>
2026-04-16 22:44:34 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Quiz
+{
+    static void Main()
+    {
+        int a = 5;
+        int b = 9;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        Console.WriteLine("a={0} and b={1}",a,b);
+    }
+}
